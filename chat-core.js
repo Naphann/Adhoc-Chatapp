a7b1634771be8038f5bcbc3d5c0acba3ec4994c8
Naphan Benchasattabuse <naphanben@gmail.com>
2016-11-09 13:13:59 +0700

    add core file

---
+++ chat-core.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const net = require('net');
+const express = require('express');
+
+const app = express();
+
+var serverlist;
+
+app.get('/', (req, res) => {
+    res.render();
+});
+
+app.get('/discover/:ip', (req, res) => {
+    // send self ip to the system for route discover
+});
+
+app.listen(3000, () => {
+    console.log(`server is listening on port 3000`);
+});
